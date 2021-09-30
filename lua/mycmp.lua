c794c7df3123634c1bdeb1f6e43b816ba04f48f6
Eorclus <aldzikridp@gmail.com>
2021-09-30 19:22:49 +0700

    snippet

--- lua/mycmp.lua
+++ lua/mycmp.lua
@@ -30,7 +30,7 @@ cmp.setup {
       if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
       elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes(luasnip.expand_or_jump, true, true, true), '')
+        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -39,7 +39,7 @@ cmp.setup {
       if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
       elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes(luasnip.jump(-1), true, true, true), '')
+        luasnip.jump(-1)
       else
         fallback()
       end
@@ -47,36 +47,14 @@ cmp.setup {
   },
 
   sources = {
-    { name = "luasnip" },
     { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "luasnip" },
     { name = "buffer" },
     { name = "nvim_lua" },
-    { name = "path" },
   },
 
   documentation = {
       border = "single"
     }
 }
-
---_G.expandOrJump = function ()
---    if vim.fn.pumvisible() == 1 then
---        return t "<C-n>"
---    elseif luasnip and luasnip.expand_or_jumpable() then
---        return luasnip.expand_or_jump()
---    end
---    return
---end
---
---set_keymap("i", "<C-K>", "v:lua.expandOrJump()", { expr = true, noremap = true})
---
---luasnipJump = function(direction)
---    if luasnip.jumpable(direction) then
---        luasnip.jump(direction)
---    end
---    return
---end
---
---set_keymap("i", "<C-J>", ":lua luasnipJump(-1)<CR>", { expr = true, noremap = true})
---set_keymap("s", "<C-K>", ":lua luasnipJump(1)<CR>", { expr = true, noremap = true})
---set_keymap("s", "<C-J>", ":lua luasnipJump(-1)<CR>", { expr = true, noremap = true})
