597d48fe3baa55bd447db0daf7af9ccb2cd6c3dd
Eorclus <aldzikridp@gmail.com>
2021-11-15 09:50:17 +0700

    indentline option

--- lua/myindentline.lua
+++ lua/myindentline.lua
@@ -3,7 +3,7 @@ vim.opt.list = true
 require("indent_blankline").setup {
     use_treesitter = true,
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
     show_trailing_blankline_indent = false,
     buftype_exclude = {'terminal', 'nofile'},
     filetype_exclude = {'help', 'lspinfo', 'checkhealth', 'markdown', ''},
