4e8c098c717e63e2e9b363026dec8563a60c0d5a
Eorclus <aldzikridp@gmail.com>
2022-02-15 13:06:30 +0700

    add function to get node type

--- lua/myindentline.lua
+++ lua/myindentline.lua
@@ -7,6 +7,8 @@ require("indent_blankline").setup {
     show_trailing_blankline_indent = false,
     buftype_exclude = {'terminal', 'nofile'},
     filetype_exclude = {'help', 'lspinfo', 'checkhealth', 'markdown', ''},
+    -- To get pattern below cursor use
+  -- lua print(require 'nvim-treesitter.ts_utils'.get_node_at_cursor():type())
     context_patterns = {
         'export_clause',
         'object',
@@ -23,8 +25,12 @@ require("indent_blankline").setup {
         'catch_clause',
         'arguments',
         'array',
+        'expression_statement',
     },
 }
+function _G.getNodeType()
+  print(require 'nvim-treesitter.ts_utils'.get_node_at_cursor():type())
+end
 vim.api.nvim_command("highlight! link IndentBlanklineChar LineNr")
 vim.api.nvim_command("highlight! IndentBlanklineContextChar guifg=#7aa2f7")
 vim.api.nvim_command("highlight! IndentBlanklineContextStart guisp=#7aa2f7 gui=underline")
