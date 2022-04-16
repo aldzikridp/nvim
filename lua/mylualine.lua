dedc068509f4b14849cb2dc16bbd9abcf1006778
Eorclus <aldzikridp@gmail.com>
2022-04-16 21:12:43 +0700

    use global status

--- lua/mylualine.lua
+++ lua/mylualine.lua
@@ -1,16 +1,17 @@
 require('lualine').setup{
 options = {
-          theme = 'tokyonight',
-          icons_enabled = true,
+  theme = 'tokyonight',
+  icons_enabled = true,
+  globalstatus = true,
 },
 sections = {
-    lualine_a = { {'mode', upper = true} },
-              lualine_b = { {'branch', icon = ''}},
-              lualine_c = { {'filename', file_status = true} ,{'diagnostics', sources = { 'nvim_diagnostic'}}},
-              lualine_x = { 'encoding', 'fileformat', 'filetype' },
-              lualine_y = { 'progress' },
-              lualine_z = { 'location'  },
-      }
+  lualine_a = { {'mode', upper = true} },
+    lualine_b = { {'branch', icon = ''}},
+    lualine_c = { {'filename', file_status = true} ,{'diagnostics', sources = { 'nvim_diagnostic'}}},
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location'  },
+  }
 }
 --lualine.options.'diagnostics' = {
 --    sources = 'nvim_lsp',
