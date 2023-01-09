4445a96f689ff6bba667c5722ea4b5a0356e6214
Eorclus <aldzikridp@gmail.com>
2023-01-09 18:47:46 +0700

    update config

--- lua/myfzf.lua
+++ lua/myfzf.lua
@@ -2,8 +2,14 @@ local function setKeymap(key, command)
     return vim.api.nvim_set_keymap("n", key, command, {noremap = true, silent = true})
 end
 
+require'fzf-lua'.setup {
+  grep = { multiprocess = true },
+  files = { multiprocess = true },
+  git = { files = { multiprocess = true } },
+}
+
 setKeymap("<leader>ff", "<Cmd>lua require('fzf-lua').files()<CR>")
-setKeymap("<leader>fg", "<Cmd>lua require('fzf-lua').live_grep()<CR>")
+setKeymap("<leader>fg", "<Cmd>lua require('fzf-lua').live_grep_native()<CR>")
 setKeymap("<leader>fc", "<Cmd>lua require('fzf-lua').lgrep_curbuf()<CR>")
 setKeymap("<leader>fb", "<Cmd>lua require('fzf-lua').buffers()<CR>")
 setKeymap("<leader>fh", "<Cmd>lua require('fzf-lua').help_tags()<CR>")
