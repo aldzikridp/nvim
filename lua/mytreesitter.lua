bddbf8875b10acec09c765075cd822bff35ab981
Eorclus <aldzikridp@gmail.com>
2023-01-09 18:47:32 +0700

    disable textobject, because breaking fzf

--- lua/mytreesitter.lua
+++ lua/mytreesitter.lua
@@ -2,7 +2,10 @@ require"nvim-treesitter.install".compilers = {"gcc"}
 --require 'nvim-treesitter.install'.compilers = { 'clang++'}
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
-    highlight = {enable = true},
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
     indent = {enable = true},
     incremental_selection = {
        enable = true,
@@ -17,48 +20,48 @@ ts.setup {
     highlight_definitions = { enable = true },
     highlight_current_scope = { enable = false },
   },
-  textobjects = {
-    select = {
-        enable = true,
-
-        -- Automatically jump forward to textobj, similar to targets.vim 
-        lookahead = true,
-
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-
-          ["iF"] = {
-            python = "(function_definition) @function",
-            cpp = "(function_definition) @function",
-            c = "(function_definition) @function",
-            java = "(method_declaration) @function",
-          },
-        },
-      },
-       move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
-    },
-  },
+--  textobjects = {
+--    select = {
+--        enable = true,
+--
+--        -- Automatically jump forward to textobj, similar to targets.vim 
+--        lookahead = true,
+--
+--        keymaps = {
+--          -- You can use the capture groups defined in textobjects.scm
+--          ["af"] = "@function.outer",
+--          ["if"] = "@function.inner",
+--          ["ac"] = "@class.outer",
+--          ["ic"] = "@class.inner",
+--
+--          ["iF"] = {
+--            python = "(function_definition) @function",
+--            cpp = "(function_definition) @function",
+--            c = "(function_definition) @function",
+--            java = "(method_declaration) @function",
+--          },
+--        },
+--      },
+--       move = {
+--      enable = true,
+--      set_jumps = true, -- whether to set jumps in the jumplist
+--      goto_next_start = {
+--        ["]m"] = "@function.outer",
+--        ["]]"] = "@class.outer",
+--      },
+--      goto_next_end = {
+--        ["]M"] = "@function.outer",
+--        ["]["] = "@class.outer",
+--      },
+--      goto_previous_start = {
+--        ["[m"] = "@function.outer",
+--        ["[["] = "@class.outer",
+--      },
+--      goto_previous_end = {
+--        ["[M"] = "@function.outer",
+--        ["[]"] = "@class.outer",
+--      },
+--    },
+--  },
 }
 
