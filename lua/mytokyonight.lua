37524cde1cdf20f472fa6d706c988d82dc9eb8ad
Eorclus <aldzikridp@gmail.com>
2023-06-15 10:57:46 +0700

    migrate tokyonight settings to lua

--- lua/mytokyonight.lua
+++ lua/mytokyonight.lua
@@ -1,4 +1,7 @@
+require("tokyonight").setup({
+  hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = true, -- dims inactive windows
+})
 vim.cmd [[colorscheme tokyonight]]
 --vim.cmd [[highlight NormalFloat guifg='#c0caf5']]
 --vim.cmd [[highlight FloatBorder guifg='#c0caf5']]
-vim.g.tokyonight_hide_inactive_statusline = true
