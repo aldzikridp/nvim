c867b1f775fa3a4002d78f691add141de007059b
aldzikridp <aldzikridp@gmail.com>
2024-02-13 11:23:34 +0700

    cleanup unused plugins

--- init.lua
+++ init.lua
@@ -2,18 +2,14 @@ if vim.loop.os_uname().version:match("NixOS") ~= "NixOS" then
   require 'mypaq'
 end
 require 'mybase'
---require 'myplugins'
 require 'mytokyonight'
 require 'mybufferline'
 require 'myluatree'
 require 'mytreesitter'
 require 'myfzf'
 require 'mylsp'
---require 'mynullls'
 require 'mycmp'
 require 'mylualine'
 require 'myindentline'
 require 'myhttp'
 require 'gitsigns'.setup()
---require 'myshade'
-vim.o.guifont = "JetBrainsMono Nerd Font:h11"
