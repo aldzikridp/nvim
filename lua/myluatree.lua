eeb05cbc145cf23ae1265c91a50afbd85392be4a
aldzikridp <aldzikridp@gmail.com>
2024-02-13 11:17:05 +0700

    open current file in nvim-tree

--- lua/myluatree.lua
+++ lua/myluatree.lua
@@ -2,9 +2,12 @@ require'nvim-tree'.setup{
     hijack_cursor = true,
     diagnostics = { enable = true },
     renderer = {
-    indent_markers = {
+      indent_markers = {
+        enable = true;
+      },
+    },
+    update_focused_file = {
       enable = true;
     },
-  },
 }
 vim.api.nvim_set_keymap("n", "<leader>b", "<Cmd>NvimTreeToggle<CR>", {noremap = true, silent = true})
