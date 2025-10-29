62965d43109af1eac56a6dec1071256b5a0134c6
aldzikridp <aldzikridp@gmail.com>
2025-10-29 10:58:42 +0700

    remove comments

--- lua/mylsp-ng.lua
+++ lua/mylsp-ng.lua
@@ -132,43 +132,12 @@ vim.diagnostic.config {
 --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. Available keys are:
---  - cmd (table): Override the default command used to start the server
---  - filetypes (table): Override the default list of associated filetypes for the server
---  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
---  - settings (table): Override the default settings passed when initializing the server.
---        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 local langservers = {
   'ccls'
   --,'clangd'
   ,'nixd'
   ,'dockerls'
-
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`ts_ls`) will work just fine
-  -- ts_ls = {},
-  --
-
 }
---for _, langserver in ipairs(langservers) do
---  require('lspconfig')[langserver].setup {
---    capabilities = capabilities,
---  }
---  vim.lsp.config.[langserver] = {
---    capabilities = capabilities
---  }
---end
 vim.lsp.config('*', {
   capabilities = capabilities,
 })
